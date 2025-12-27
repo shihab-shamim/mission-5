09493ee1894f89bb2b70f697bfafdcdbbd8ac7b7
shihab-shamim <shihabshamim767@gmail.com>
2025-12-27 19:08:07 +0600

    prisma all comment out

--- prisma-setup-2/crud.ts
+++ prisma-setup-2/crud.ts
@@ -2,73 +2,74 @@
 import { prisma } from "./lib/prisma";
 
 async function run(){
-    // const createUser=await prisma.user.create({
-    //     data:{
-    //         name:"mitu",
-    //         email:"mitu@prisma.io",
-    //     }
-    // })
-    // console.log("Created user:",createUser);
-//     const createPost=await prisma.post.create({
-//         data:{
-//             title:"Hello shihab",
-//             content:"This is my first post!(shihab)",
-//             published:true,
-//             userId:3,
-//         }
-//     })
-// console.log("Created post:",createPost);
+    const createUser=await prisma.user.create({
+        data:{
+            name:"mitu",
+            email:"mitu@prisma.io",
+        }
+    })
+    console.log("Created user:",createUser);
+    const createPost=await prisma.post.create({
+        data:{
+            title:"Hello shihab",
+            content:"This is my first post!(shihab)",
+            published:true,
+            userId:3,
+        }
+    })
+console.log("Created post:",createPost);
 
-// const createProfile = await prisma.profile.create({
-//     data:{
-//         bio:"I am a software developer",
-//         userId:3,
-//     }
-// })
+const createProfile = await prisma.profile.create({
+    data:{
+        bio:"I am a software developer",
+        userId:3,
+    }
+})
+console.log("Created profile:",createProfile);
 // rettive all user 
-// const getAllUser=await prisma.user.findMany({
-//     include:{
-//         posts:true,
-//         profiles:true,
-//     },
-//     // select:{
-//     //     posts:true,
-//     //     profiles:true,
-//     //     name:true,
-//     //     email:true,
-//     // }
-// })
+const getAllUser=await prisma.user.findMany({
+    include:{
+        posts:true,
+        profiles:true,
+    },
+    // select:{
+    //     posts:true,
+    //     profiles:true,
+    //     name:true,
+    //     email:true,
+    // }
+})
 
-// console.dir(getAllUser,{depth:Infinity});
+console.dir(getAllUser,{depth:Infinity});
 
 
-// const updateUser = await prisma.user.update({
-//   where: {
-//     id: 3,
-//   },
-//   data: {
-//     name: 'update user',
-//   },
-//   select:{
-//     id:true,
-//     name:true,
-//     email:true,
-//     posts:true,
-//     profiles:true,
+const updateUser = await prisma.user.update({
+  where: {
+    id: 3,
+  },
+  data: {
+    name: 'update user',
+  },
+  select:{
+    id:true,
+    name:true,
+    email:true,
+    posts:true,
+    profiles:true,
   
-//   }
+  }
   
-// })
-// console.log("Updated user:",updateUser);
+})
+console.log("Updated user:",updateUser);
 
 // delete user and recode delete
-// const deleteUser=await prisma.user.delete({
-//     where:{
-//         id:1,
-//     },
+const deleteUser=await prisma.user.delete({
+    where:{
+        id:1,
+    },
    
-// })
-// console.log("Deleted user:",deleteUser);
+})
+console.log("Deleted user:",deleteUser);
 
 
 // upsert ta hocche  paile update hobe naile create hobe
